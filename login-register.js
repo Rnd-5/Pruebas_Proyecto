bf55aec72b281b8c0f29887b951be2b4dc7611ad
Rnd-5 <34996599+Rnd-5@users.noreply.github.com>
2018-06-12 17:15:55 -0400

    Proyecto_Geat-a-Job version 0.002-Rond

--- login-register.js
+++ login-register.js
@@ -1,11 +1,4 @@
-/*
- *
- * login-register modal
- * Autor: Creative Tim
- * Web-autor: creative.tim
- * Web script: http://creative-tim.com
- * 
- */
+
 function showRegisterForm(){
     $('.loginBox').fadeOut('fast',function(){
         $('.registerBox').fadeIn('fast');
@@ -17,8 +10,11 @@ function showRegisterForm(){
     $('.error').removeClass('alert alert-danger').html('');
        
 }
+
 function showLoginForm(){
     $('#loginModal .registerBox').fadeOut('fast',function(){
+        $('.registroSoliciante').fadeOut('fast'); //Oculta el from. del solicitante
+        $('.registroEmpleador').fadeOut('fast'); //Oculta el from. del empleador
         $('.loginBox').fadeIn('fast');
         $('.register-footer').fadeOut('fast',function(){
             $('.login-footer').fadeIn('fast');    
@@ -26,24 +22,43 @@ function showLoginForm(){
         
         $('.modal-title').html('Login with');
     });       
-     $('.error').removeClass('alert alert-danger').html(''); 
+     $('.error').removeClass('alert alert-danger').html('');
 }
+//===MUESTRA LOS FORMULARIOS DEL SOLICITANTE Y DEL EMPLEADOR===============//
+function showRegisterForm_Solicitante(){
+     $('.registerBox').fadeOut('fast',function(){
+        $('.registroSoliciante').fadeIn('fast');
+        $('.login-footer').fadeOut('fast',function(){
+            $('.register-footer').fadeIn('fast');
+        });
+        $('.modal-title').html('Register with');
+    }); 
+    $('.error').removeClass('alert alert-danger').html('');
+}
+function showRegisterForm_Empleador(){
+     $('.registerBox').fadeOut('fast',function(){
+        $('.registroEmpleador').fadeIn('fast');
+        $('.login-footer').fadeOut('fast',function(){
+            $('.register-footer').fadeIn('fast');
+        });
+        $('.modal-title').html('Register with');
+    }); 
+    $('.error').removeClass('alert alert-danger').html('');
+}
+//========================================================================//
 
 function openLoginModal(){
     showLoginForm();
     setTimeout(function(){
         $('#loginModal').modal('show');    
-    }, 230);
-    
+    }, 230); 
 }
 function openRegisterModal(){
     showRegisterForm();
     setTimeout(function(){
         $('#loginModal').modal('show');    
     }, 230);
-    
 }
-
 function loginAjax(){
     /*   Remove this comments when moving to server
     $.post( "/login", function( data ) {
@@ -54,11 +69,9 @@ function loginAjax(){
             }
         });
     */
-
 /*   Simulate error message from the server   */
      shakeModal();
 }
-
 function shakeModal(){
     $('#loginModal .modal-dialog').addClass('shake');
              $('.error').addClass('alert alert-danger').html("Invalid email/password combination");
